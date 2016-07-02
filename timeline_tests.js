d7cc6db033b6ef3ed9f47e3b9352b21074193b8a
h1-the-swan <jason.portenoy@gmail.com>
2016-07-01 18:09:49 -0700

    refactoring so that each year has its own group element

--- timeline_tests.js
+++ timeline_tests.js
@@ -5,18 +5,12 @@ var json_fname = 'Climate_change.json'
 d3.json(json_fname, function(error, data_total) {
 	data_total.forEach(function(d) {
 		d.lane = 0;
-		d.id = d.title;
-		// d.start = +d.pubdate.split('-')[0];
-		// d.end = d.start + 1;
-		// d.start = new Date(d.pubdate);
-		// d.end = new Date(d.pubdate);
-		// d.end = new Date(d.end.setFullYear(d.end.getFullYear()+5));
-		d.start = d.year;
-		d.end = d.year+1;
 	});
 	var dataByYear = d3.nest()
 						.key(function(d) {return d.year;})
-						.sortValues(function(a, b) {return d3.descending(a.eigenfactor_score, b.eigenfactor_score);})
+						.sortValues(function(a, b) {
+							return d3.descending(a.eigenfactor_score, b.eigenfactor_score);
+						})
 						// .map(data_total, d3.map);
 						.entries(data_total);
 	dataByYear.forEach(function(d) {
@@ -29,19 +23,19 @@ d3.json(json_fname, function(error, data_total) {
 	data = data_total.sort(function(a, b) {
 			return d3.descending(a.eigenfactor_score, b.eigenfactor_score); 
 		}).slice(0,25);
-	// var minYear = d3.min(data, function(d) { return d.start; }).getFullYear();
-	// console.log(minYear);
 	var lanes = ["Climate change"],
 				laneLength = lanes.length,
 			// timeBegin = new Date(String(minYear-1)),
-			timeBegin = d3.min(data, function(d) { return d.start; }),
-			timeEnd = d3.max(data, function(d) { return d.end; });
+			timeBegin = +d3.min(dataByYear, function(d) { return d.year; }) - 1,
+			timeEnd = +d3.max(dataByYear, function(d) { return d.year; }) + 1;
 
 		var m = [20, 15, 15, 120], //top right bottom left
 			w = 960 - m[1] - m[3],
 			h = 350 - m[0] - m[2],
 			miniHeight = laneLength * 12 + 30,
 			mainHeight = h - miniHeight - 50;
+
+		var mainMinRad = 10;
 
 		var stylesBase = {
 			'opacity': .2
@@ -52,11 +46,9 @@ d3.json(json_fname, function(error, data_total) {
 
 		//scales
 		var x = d3.scale.linear()
-		// var x = d3.time.scale()
 				.domain([timeBegin, timeEnd])
 				.range([0, w]);
 		var x1 = d3.scale.linear()
-		// var x1 = d3.time.scale()
 				.range([0, w]);
 		var y1 = d3.scale.linear()
 				.domain([0, laneLength])
@@ -70,7 +62,6 @@ d3.json(json_fname, function(error, data_total) {
 		var efSumScale = d3.scale.linear()
 				.domain(d3.extent(dataByYear, function(d) { return d.sum_eigenfactor; }))
 				.range([0, 5]);
-		console.log(d3.extent(dataByYear, function(d) { return d.sum_eigenfactor; }))
 
 		var chart = d3.select("body")
 					.append("svg")
@@ -97,6 +88,7 @@ d3.json(json_fname, function(error, data_total) {
 					.attr("class", "mini");
 		
 		//main lanes and texts
+		// TODO lane lines are weird. draws one line per data point?
 		main.append("g").selectAll(".laneLines")
 			.data(data)
 			.enter().append("line")
@@ -136,6 +128,7 @@ d3.json(json_fname, function(error, data_total) {
 			.attr("text-anchor", "end")
 			.attr("class", "laneText");
 
+		// Axes
 		var xAxisMini = d3.svg.axis()
 			.orient("bottom")
 			// .ticks(5)
@@ -156,27 +149,29 @@ d3.json(json_fname, function(error, data_total) {
 			.attr("class", "xaxis")
 			.call(xAxisMain);
 
-		var itemRects = main.append("g")
+		var mainClipPath = main.append("g")
 							.attr("clip-path", "url(#clip)");
 		
-		//mini item rects
+		//mini items
 		// note: mouseover events will not play well with the brush
-		var miniItems = mini.append("g").selectAll("miniItems")
+		var miniItems = mini.append("g").selectAll(".miniItem")
 			.data(data_total)
-			// .enter().append("rect")
-			.enter().append("circle")
-			.attr("class", function(d) {return "miniItem" + d.lane;})
-			// .attr("x", function(d) {return x(d.start);})
-			// .attr("y", function(d) {return y2(d.lane + .5) - 5;})
-			.attr("cx", function(d) {return x(d.start);})
-			// .attr("cy", function(d) {return y2(d.lane + .5) - 5;})
-			// .attr("width", function(d) { return x(d.end) - x(d.start);})
-			// .attr("height", 10);
-			.attr("r", function(d) {
-					d.radius = 5 + efScale(d.eigenfactor_score);
-					return d.radius;
-				})
-			.style(stylesBase);
+			.enter().append("g")
+			.attr("class", "miniItem")
+			.attr("transform", function(d) {
+				d.x = x(d.year);
+				d.y = 0;  // for now
+				d.radius = 5 + efScale(d.eigenfactor_score);
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+			// .enter().append("circle")
+			// .attr("class", function(d) {return "miniItem" + d.lane;})
+			// .attr("cx", function(d) {return x(d.start);})
+			// .attr("r", function(d) {
+			// 		d.radius = 5 + efScale(d.eigenfactor_score);
+			// 		return d.radius;
+			// 	})
+			// .style(stylesBase);
 
 		function stackItems(items, scale) {
 			var yearsList = [];
@@ -185,22 +180,70 @@ d3.json(json_fname, function(error, data_total) {
 					yearsList.push(d.year);
 				}
 			});
+			console.log(items);
 			var maxRad = d3.max(items[0], function(d) {return d.__data__.radius});
 			for (var i = 0, len = yearsList.length; i < len; i++) {
 				thisYearMini = items.filter(function(d) {return d.year==yearsList[i]});
-				var cy = 0;
+				var y = 0;
 				thisYearMini.each(function(d) {
-					if (cy == 0) {
-						cy = scale(d.lane) + maxRad;
+					if (y == 0) {
+						y = scale(d.lane) + maxRad;
 					} else {
-						cy = cy + 2*d.radius;
+						y = y + 2*d.radius;
 					}
-					d.cy = cy;
-					d3.select(this).attr("cy", cy)
+					d.y = y;
+					d3.select(this).attr("transform", function(d) {
+						return "translate(" + d.x + "," + d.y + ")";
+					});
 				});
 			}
 		}
 		stackItems(miniItems, y2);
+
+		var miniMarks = miniItems.append("circle")
+			.attr("class", "miniMark")
+			.attr("r", function(d) {return d.radius;})
+			.style(stylesBase);
+
+		//main items
+		var yearItems = mainClipPath.append("g").selectAll(".yearItem")
+			.data(dataByYear)
+			.enter().append("g")
+			.attr("class", "yearItem")
+			.attr("transform", function(d) {
+				d.x = 0;  //for now
+				d.y = 0;  //for now
+				d.radius = mainMinRad + (2 * efSumScale(d.sum_eigenfactor));
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+
+		var yearMarks = yearItems.append("circle")
+			.attr("class", "yearMark")
+			.on('mouseover', expand)
+			.on('mouseout', contract)
+			.style(stylesVisible);
+
+		//label for number of papers
+		yearItems.append("text")
+			.attr("text-anchor", "middle")
+			.attr("y", ".3em")  //nudge
+			.attr("class", "numIndicator")
+			.text(function(d) {return d.values.length;});
+
+		var paperItems = yearItems.append("g").selectAll(".paperItem")
+			.data(function(d) {return d.values})
+			.enter().append("g")
+			.attr("class", "paperItem")
+			.attr("transform", function(d) {
+				d.x = 0;  //for now
+				d.y = 0;  //for now
+				d.radius = mainMinRad + (2 * efSumScale(d.sum_eigenfactor));
+				return "translate(" + d.x + "," + d.y + ")";
+			});
+
+		var paperMarks = paperItems.append("circle")
+			.attr("r", 0)  //for now
+			.attr("class", "paperMark");
 
 		var paperItemX = function(d, i) {
 			return x1(+d.year) + ((i*i) * 3);
@@ -209,6 +252,7 @@ d3.json(json_fname, function(error, data_total) {
 			var rad = d.radius;
 			return y1(d.lane) + 2.2*rad*i+5*rad;
 		};
+
 
 		//mini labels
 		// mini.append("g").selectAll(".miniLabels")
@@ -243,11 +287,14 @@ d3.json(json_fname, function(error, data_total) {
 
 		
 		function display() {
-			var marks, labels,
-				minExtent = brush.extent()[0],
+			var minExtent = brush.extent()[0],
 				maxExtent = brush.extent()[1],
 				// visItems = data.filter(function(d) {return d.start < maxExtent && d.start > minExtent;});
-				visItems = dataByYear.filter(function(d) {return d.year < maxExtent && d.year > minExtent;});
+				// visItems = dataByYear.filter(function(d) {return d.year < maxExtent && d.year > minExtent;});
+				visItems = yearItems.filter(function(d) {return d.year < maxExtent && d.year > minExtent;})
+				notVisItems = yearItems.filter(function(d) {return d.year>= maxExtent || d.year <= minExtent;});
+			visItems.style("display", "");
+			notVisItems.style("display", "none");
 
 			mini.select(".brush")
 				.call(brush.extent([minExtent, maxExtent]));
@@ -269,40 +316,20 @@ d3.json(json_fname, function(error, data_total) {
 				}).style(stylesVisible);
 
 			//update main item marks
-			var rad = 5;
-			marks = itemRects.selectAll(".yearItem")
-				.attr("cx", function(d) {return x1(+d.year);})
-				.attr("cy", function(d) {d.cy = y1(d.lane)+ rad; return d.cy; })
-			        // .data(visItems, function(d) { return d.id; })
-			        .data(visItems);
-				// .attr("x", function(d) {return x1(d.start);})
-				// .attr("width", function(d) {return x1(d.end) - x1(d.start);});
-			
-			marks.enter().append("circle")
-				.attr("class", "yearItem mainItem")
-				.attr("cx", function(d) {d.cx = x1(+d.year); return d.cx;})
-				.attr("cy", function(d) {d.cy = y1(d.lane)+ rad; return d.cy; })
-				.attr('r', function(d) {
-						d.radius = rad + (2 * efSumScale(d.sum_eigenfactor));
-						// d.radius = rad;
-						return d.radius;
-					})
-				.on('mouseover', expand)
-				// .on('mouseout', function() {$('.yearItemLabel').show();})
-				.on('mouseout', contract)
-				.style(stylesVisible);
-				// .attr("width", function(d) {return x1(d.end) - x1(d.start);})
-				// .attr("height", function(d) {return .8 * y1(1);});
+			visItems.attr("transform", function(d) {
+				d.x = x1(d.year);
+				d.y = y1(d.lane) + mainMinRad;
+				return "translate(" + d.x + "," + d.y + ")";
+			});
 
-			marks.exit().remove();
-			// stackItems(marks, y1);
-			//
-			var paperItems = itemRects.selectAll(".paperItem")
-				// .attr("cx", function(d) {return x1(+d.year);});
-				.attr("cx", paperItemX);
-			var paperItemLabels = itemRects.selectAll(".paperItemLabel")
-				// .attr("x", function(d) {return x1(+d.year);});
-				.attr("x", paperItemX);
+			yearMarks.attr("r", function(d) {return d.radius;});
+
+			// var paperItems = mainClipPath.selectAll(".paperItem")
+			// 	// .attr("cx", function(d) {return x1(+d.year);});
+			// 	.attr("cx", paperItemX);
+			// var paperItemLabels = mainClipPath.selectAll(".paperItemLabel")
+			// 	// .attr("x", function(d) {return x1(+d.year);});
+			// 	.attr("x", paperItemX);
 
 			//update the item labels
 			// var rotate = -20;
@@ -388,16 +415,16 @@ d3.json(json_fname, function(error, data_total) {
             //
 			// labels.exit().remove();
 
-			var numIndicators = itemRects.selectAll(".numIndicator")
-				.attr("x", function(d) {return x1(+d.year);})
-				.attr("y", function(d) {return y1(d.lane)+ d.radius;})
-				.data(visItems);
-			numIndicators.enter().append("text")
-				.text(function(d) {return d.values.length})
-				.attr("x", function(d) {return x1(+d.year);})
-				.attr("y", function(d) {return y1(d.lane)+ d.radius;})
-				.attr("text-anchor", "middle")
-				.attr("class", "numIndicator");
+			// var numIndicators = mainClipPath.selectAll(".numIndicator")
+			// 	.attr("x", function(d) {return x1(+d.year);})
+			// 	.attr("y", function(d) {return y1(d.lane)+ d.radius;})
+			// 	.data(visItems);
+			// numIndicators.enter().append("text")
+			// 	.text(function(d) {return d.values.length})
+			// 	.attr("x", function(d) {return x1(+d.year);})
+			// 	.attr("y", function(d) {return y1(d.lane)+ d.radius;})
+			// 	.attr("text-anchor", "middle")
+			// 	.attr("class", "numIndicator");
 
 			//update axis
 			xAxisMainObj.call(xAxisMain);
@@ -419,72 +446,80 @@ d3.json(json_fname, function(error, data_total) {
 		return y1(d.lane) + 2.2*rad*i+5*rad;
 	};
 	function expand(yearData) {
-		console.log(yearData);
-		if (!yearData.expanded) {
-			
-			// d3.selectAll(".yearItemLabel").classed("hidden");
-			$( '.yearItemLabel' ).hide();
-			var dur = 500;
-			// var rad = yearData.radius;
-			var rad = 10;
-			var parentY = yearData.cy;
-			var marks = itemRects.selectAll(".paperItem")
-						// .data(visItems, function(d) { return d.id; })
-						.data(yearData.values);
-			marks.enter().append("circle")
-					// .attr("class", function(d) {return "mainItem miniItem" + d.lane;})
-					.attr("class", "paperItem")
-					.on('mouseover', function(d) {console.log(d);}).append('text').text('d');
-			// marks.exit().transition().duration(1000).attr("cy", parentY).remove();
-			// itemRects.selectAll('text').data(yearData.values).enter().append('text').attr("x", function(d) {console.log(d); return d.cx;}).attr("y", function(d) {return d.cy;}).text(function(d) {return d.title;});
-			marks.exit().remove();
-
-			var labels = itemRects.selectAll(".paperItemLabel")
-				.data(yearData.values);
-			labels.enter().append("text")
-				.attr("class", "paperItemLabel")
-				.attr("text-anchor", "end");
-			labels.exit().remove();
-
-			marks.attr("cx", beforeTransitionX)
-					.attr('r', function(d) {
-							// d.radius = rad + (2 * efSumScale(d.sum_eigenfactor));
-							d.radius = rad + (2 * efScale(d.eigenfactor_score));
-							return d.radius;
-						})
-					.attr("cy", beforeTransitionY)
-					.transition().duration(dur)
-					.attr("cx", afterTransitionX)
-					.attr("cy", afterTransitionY)
-					.style(stylesVisible);
-			labels.attr("x", beforeTransitionX)
-				.text(function(d) {return d.title;})
-				.attr("y", beforeTransitionY)
-				.transition().duration(dur)
-				.attr("x", afterTransitionX)
-				.attr("y", afterTransitionY);
-		yearData.expanded = true;
-		}
-	}
-
-	function contract(d) {
 		var dur = 500;
-		if (d.expanded) {
-			var marks = itemRects.selectAll(".paperItem")
-				.interrupt("contract")
-				.transition("contract").delay(2000).duration(dur)
-				.attr("cx", beforeTransitionX)
-				.attr("cy", 0)
-				.each("end", function(_, i) {
-					if (i === 0) d.expanded = false;
-					})
-				.remove();
-			var labels = itemRects.selectAll(".paperItemLabel")
-				.interrupt("contract")
-				.transition("contract").delay(2000).duration(dur)
-				.attr("x", beforeTransitionX)
-				.attr("y", 0)
-				.remove();
-		}
+		var sel = paperItems.filter(function(d) {return d.year===yearData.year});
+		console.log(sel);
 	}
+	function contract(yearData) {
+		//
+	}
+	// function expand(yearData) {
+	// 	console.log(yearData);
+	// 	if (!yearData.expanded) {
+	// 		
+	// 		// d3.selectAll(".yearItemLabel").classed("hidden");
+	// 		$( '.yearItemLabel' ).hide();
+	// 		var dur = 500;
+	// 		// var rad = yearData.radius;
+	// 		var rad = 10;
+	// 		var parentY = yearData.cy;
+	// 		var marks = mainClipPath.selectAll(".paperItem")
+	// 					// .data(visItems, function(d) { return d.id; })
+	// 					.data(yearData.values);
+	// 		marks.enter().append("circle")
+	// 				// .attr("class", function(d) {return "mainItem miniItem" + d.lane;})
+	// 				.attr("class", "paperItem")
+	// 				.on('mouseover', function(d) {console.log(d);}).append('text').text('d');
+	// 		// marks.exit().transition().duration(1000).attr("cy", parentY).remove();
+	// 		// itemRects.selectAll('text').data(yearData.values).enter().append('text').attr("x", function(d) {console.log(d); return d.cx;}).attr("y", function(d) {return d.cy;}).text(function(d) {return d.title;});
+	// 		marks.exit().remove();
+    //
+	// 		var labels = mainClipPath.selectAll(".paperItemLabel")
+	// 			.data(yearData.values);
+	// 		labels.enter().append("text")
+	// 			.attr("class", "paperItemLabel")
+	// 			.attr("text-anchor", "end");
+	// 		labels.exit().remove();
+    //
+	// 		marks.attr("cx", beforeTransitionX)
+	// 				.attr('r', function(d) {
+	// 						// d.radius = rad + (2 * efSumScale(d.sum_eigenfactor));
+	// 						d.radius = rad + (2 * efScale(d.eigenfactor_score));
+	// 						return d.radius;
+	// 					})
+	// 				.attr("cy", beforeTransitionY)
+	// 				.transition().duration(dur)
+	// 				.attr("cx", afterTransitionX)
+	// 				.attr("cy", afterTransitionY)
+	// 				.style(stylesVisible);
+	// 		labels.attr("x", beforeTransitionX)
+	// 			.text(function(d) {return d.title;})
+	// 			.attr("y", beforeTransitionY)
+	// 			.transition().duration(dur)
+	// 			.attr("x", afterTransitionX)
+	// 			.attr("y", afterTransitionY);
+	// 	yearData.expanded = true;
+	// 	}
+	// }
+    //
+	// function contract(d) {
+	// 	var dur = 500;
+	// 	if (d.expanded) {
+	// 		var marks = mainClipPath.selectAll(".paperItem")
+	// 			.interrupt("contract")
+	// 			.transition("contract").delay(2000).duration(dur)
+	// 			.attr("cx", beforeTransitionX)
+	// 			.attr("cy", 0)
+	// 			.each("end", function(_, i) {
+	// 				if (i === 0) d.expanded = false;
+	// 				})
+	// 			.remove();
+	// 		var labels = mainClipPath.selectAll(".paperItemLabel")
+	// 			.interrupt("contract")
+	// 			.transition("contract").delay(2000).duration(dur)
+	// 			.attr("x", beforeTransitionX)
+	// 			.attr("y", 0)
+	// 			.remove();
+	// 	}
+	// }
 });
